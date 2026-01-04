6501fb81260240b32b9bc22cc6cc107ac1f61aa5
SmilingPixel <smiling.pixel.1f642@outlook.com>
2026-01-04 01:47:49 +0000

    feat: support fetching and rendering local images

--- composeApp/src/commonMain/kotlin/io/github/smiling_pixel/ImageLoader.kt
+++ composeApp/src/commonMain/kotlin/io/github/smiling_pixel/ImageLoader.kt
@@ -4,10 +4,12 @@ import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.network.ktor3.KtorNetworkFetcherFactory
 import coil3.request.crossfade
+import io.github.smiling_pixel.filesystem.LocalFileFetcher
 
 fun getAsyncImageLoader(context: PlatformContext): ImageLoader {
     return ImageLoader.Builder(context)
         .components {
+            add(LocalFileFetcher.Factory()) // use localfile schema, e.g., `localfile://myimage.jpg`
             add(KtorNetworkFetcherFactory())
         }
         .crossfade(true)
