19ab7a7970da66627a72de08a218f5d962dfb746
SmilingPixel <smiling.pixel.1f642@outlook.com>
2026-01-21 01:05:47 +0000

    feat: update package name for Android integration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -123,11 +123,11 @@ room {
 }
 
 android {
-    namespace = "io.github.smiling_pixel"
+    namespace = "io.github.smiling_pixel.mark_day"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "io.github.smiling_pixel"
+        applicationId = "io.github.smiling_pixel.mark_day"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
@@ -163,7 +163,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "io.github.smiling_pixel"
+            packageName = "io.github.smiling_pixel.mark_day"
             packageVersion = "1.0.0"
         }
     }
